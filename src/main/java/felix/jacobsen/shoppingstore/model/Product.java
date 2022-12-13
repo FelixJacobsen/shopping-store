5b191ed88594b518b6c7697dbe8abd3567d9586c
FelixJacobsen <felix.jacobsen@iths.se>
2022-12-13 12:30:06 +0100

    Create product class

---
+++ src/main/java/felix/jacobsen/shoppingstore/model/Product.java
@@ -0,0 +1,102 @@
+package felix.jacobsen.shoppingstore.model;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private BigDecimal price;
+    private Integer available;
+    private String description;
+    private String manufacturer;
+
+
+    private static class ProductBuilder{
+        private String name;
+        private BigDecimal price;
+        private String description;
+        private String manufacturer;
+
+        public ProductBuilder name(String name){
+            this.name = name;
+            return this;
+        }
+
+        private ProductBuilder price(BigDecimal price){
+            this.price = price;
+            return this;
+        }
+
+        public ProductBuilder description(String description){
+            this.description = description;
+            return this;
+        }
+
+        public ProductBuilder manufacturer(String manufacturer){
+            this.manufacturer = manufacturer;
+            return this;
+        }
+
+        public Product build(){
+            return new Product(this);
+        }
+    }
+
+    private Product(ProductBuilder productBuilder){
+        name = productBuilder.name;
+        price = productBuilder.price;
+        description = productBuilder.description;
+        manufacturer = productBuilder.manufacturer;
+    }
+    public Product(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public Integer getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(Integer available) {
+        this.available = available;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+}
---
+++ src/test/java/felix/jacobsen/shoppingstore/ShoppingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package felix.jacobsen.shoppingstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
